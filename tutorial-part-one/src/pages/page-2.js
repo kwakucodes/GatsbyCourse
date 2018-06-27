459e8ab86bd0b554ba929a66c089de0ab89d2349
kwakucodes <31509596+kwakucodes@users.noreply.github.com>
2018-06-27 16:26:21 -0400

    Create page-2.js

---
+++ tutorial-part-one/src/pages/page-2.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "gatsbu-link";
+
+export defualt () => (
+<div>
+<p> Hello world from my second page </p>
+link to <"/"> back to home </Link>
+</div>
+);
