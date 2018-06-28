d266d94cf70192e4e360f088836cc0adfae03b2a
kwakucodes <31509596+kwakucodes@users.noreply.github.com>
2018-06-27 20:27:10 -0400

    commit

--- tutorial-part-one/src/pages/counter.js
+++ tutorial-part-one/src/pages/counter.js
@@ -1,19 +1,24 @@
 import React from "react";
 
 class Counter extends React.Component {
-  constructor(){
-      super()
-      this.state = { count: 0}
+  constructor() {
+    super()
+    this.state = { count: 0 }
   }
-  
-    render() {
+
+  render() {
     return (
       <div>
         <h1>Counter</h1>
-        <p>current count: {this.state.count }</p>
-        <button onCLick = {() => this.setState({count:this.state.count +1})}
-        >plus</button>
-        <button onCLick = {() => this.setState({count:this.state.count -1})}>minus</button>
+        <p>current count: {this.state.count}</p>
+        <button onClick={() => this.setState({ count: this.state.count +
+          1 })}
+>plus
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}
+>minus
+        </button>
       </div>
     )
   }
